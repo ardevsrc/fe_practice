26a5b1defde7944180f97a3ac628391f5071bb3a
ardevsrc <dbeast@tuta.io>
2022-01-14 22:04:51 -0500

    Hero.js
    Added hero section

--- portfolio_website/src/components/Hero/Hero.js
+++ portfolio_website/src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+ <Section row nopadding>
+   <LeftSection>
+     <SectionTitle main center>
+       Welcome To <br />
+       My Personal Portfolio
+     </SectionTitle>
+     <SectionText>
+       Creating the next wave of modern web applications.
+     </SectionText>
+     <Button onClick={() => window.location = 'https://github.com/'}>Learn More</Button>
+   </LeftSection>
+ </Section>
 );
 
 export default Hero;
